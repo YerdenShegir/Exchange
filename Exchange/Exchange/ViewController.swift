16f5b9440cc776825e9340a2da6a6591b061020b
Yerden Shegir <shegirerden@gmail.com>
2017-10-03 21:54:12 +0600

    test

---
+++ Exchange/Exchange/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Exchange
+//
+//  Created by Ерден on 03.10.17.
+//  Copyright © 2017 Ерден. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
